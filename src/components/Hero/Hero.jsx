4c84d07241fac46c88b58c0c94ba34d0340a8693
MD Ahmad <waqasahmad009007@gmail.com>
2024-09-03 08:30:27 +0530

    loader added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,28 +5,56 @@ import axios, { Axios } from 'axios';
 export default function Hero() {
   const [filterData, setFilterData] = useState([])
   const [ data , setData] = useState(filterData);
+  const [isLoading, setIsLoading] = useState(false);
 
 
-  const fetchApi = ()=>{
-    // try {
-    //   const res = await axios.get('https://jsonplaceholder.typicode.com/photos');
-    //   setData(res.data)
-    //   setFilterData(res.data)
-    // } catch (error) {
-    //   console.log("error")
-    // }
-    fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(res=>res.json())
-    .then(data=>{
-      setFilterData(data)
-      setData(data)
+  const fetchApi = async()=>{
+    setIsLoading(true)
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/photos');
+      setData(res.data)
+      setFilterData(res.data)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      console.log("error")
+
     }
-    )
-    .catch(error=>console.log("error" , error))
+
   }
   useEffect(()=>{
     fetchApi()
   },[setFilterData])
+
+  if (isLoading) {
+    return (
+      <button
+        type="button"
+        className="bg-cyan-700 flex items-center gap-4 text-white py-3 px-5 rounded-xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+          />
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+        Processing...
+      </button>
+    );
+  }
 
   return (
     <div className="relative w-full">
@@ -45,9 +73,7 @@ export default function Hero() {
               <p className="mt-2 text-sm text-gray-300">
                 {item.title}
               </p>
-              <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-                View Profile &rarr;
-              </button>
+             
             </div>
           </div>
           ))}
